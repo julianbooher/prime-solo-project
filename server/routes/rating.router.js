78b060b8e6970089579a5d342909cbe245cdd454
julianbooher <julianbooher@hotmail.com>
2021-01-15 10:48:26 -0600

    route initialized and saga created

--- server/routes/rating.router.js
+++ server/routes/rating.router.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
+const { rejectUnauthenticated } = require('../modules/authentication-middleware');
 
 /**
  * GET route template
@@ -12,9 +13,43 @@ router.get('/', (req, res) => {
 /**
  * POST route template
  */
-router.post('/', (req, res) => {
-  // POST route code here
-});
+router.post('/', rejectUnauthenticated, (req, res) => {
+  console.log(req.body);
+  // RETURNING "id" will give us back the id of the created movie
+//   const insertMovieQuery = `
+//   INSERT INTO "movies" ("title", "poster", "description")
+//   VALUES ($1, $2, $3)
+//   RETURNING "id";`
+
+//   // FIRST QUERY MAKES MOVIE
+//   pool.query(insertMovieQuery, [req.body.title, req.body.poster, req.body.description])
+//   .then(result => {
+//     console.log('New Movie Id:', result.rows[0].id); //ID IS HERE!
+    
+//     const createdMovieId = result.rows[0].id
+
+//     // Depending on how you make your junction table, this insert COULD change.
+//     const insertMovieGenreQuery = `
+//       INSERT INTO "movies_genres" ("movies_id", "genres_id")
+//       VALUES  ($1, $2);
+//       `
+//       // SECOND QUERY MAKES GENRE FOR THAT NEW MOVIE
+//       pool.query(insertMovieGenreQuery, [createdMovieId, req.body.genre_id]).then(result => {
+//         //Now that both are done, send back success!
+//         res.sendStatus(201);
+//       }).catch(err => {
+//         // catch for second query
+//         console.log(err);
+//         res.sendStatus(500)
+//       })
+
+// // Catch for first query
+//   }).catch(err => {
+//     console.log(err);
+//     res.sendStatus(500)
+//   })
+res.sendStatus(201)
+})
 
 // ------------ ROUTER FROM MOVIES SAGA TO USE THE SYNTAX OF FOR RATING SUBMIT -------------
 // router.post('/', (req, res) => {
---
+++ src/redux/sagas/rating.saga.js
@@ -0,0 +1,60 @@
+import axios from 'axios';
+import {put, takeLatest } from 'redux-saga/effects';
+
+  // // POST ROUTE
+  function* addRating (action) {
+    try {
+      const config = {
+        headers: { 'Content-Type': 'application/json' },
+        withCredentials: true,
+      };
+  
+      yield axios.post('api/rating', action.payload, config);
+      // yield put({ type: 'FETCH_SHELF' });
+    } catch (error) {
+      console.log('Shelf post request failed', error);
+    }
+  }
+
+  // // DELETE ROUTE
+  // function* deleteItem (action) {
+  //   try {
+  //     const config = {
+  //       headers: { 'Content-Type': 'application/json' },
+  //       withCredentials: true,
+  //     };
+    
+  //     yield axios.delete(`api/shelf`, {
+  //       params: {
+  //         itemId: action.payload.itemId,
+  //         userId: action.payload.userId
+  //       }
+  //     }, config);
+
+  //     yield put({ type: 'FETCH_SHELF' });
+  //   } catch (error) {
+  //     console.log('Shelf delete request failed', error);
+  //   }
+  // }
+
+  // // PUT ROUTE
+  // function* editItem (action) {
+  //   console.log('editItem', action.payload);
+  //   try {
+  //     const config = {
+  //       headers: { 'Content-Type': 'application/json' },
+  //       withCredentials: true,
+  //     };
+    
+  //     yield axios.put(`api/shelf/${action.payload.id}`, action.payload, config);
+  //     yield put({ type: 'FETCH_SHELF' });
+  //   } catch (error) {
+  //     console.log('Shelf put request failed', error);
+  //   }
+  // }
+  
+  function* ratingSaga() {
+    yield takeLatest('ADD_RATING', addRating);
+  }
+  
+  export default ratingSaga;
