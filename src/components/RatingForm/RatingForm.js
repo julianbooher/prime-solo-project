fb2432e4eee85acaa7a7224c5d4d2917bdda4c05
julianbooher <julianbooher@hotmail.com>
2021-01-15 11:51:48 -0600

    post route and refresh done

--- src/components/RatingForm/RatingForm.js
+++ src/components/RatingForm/RatingForm.js
@@ -24,7 +24,7 @@ const styles = theme => ({
     padding: theme.spacing(2),
     margin: theme.spacing(2),
     minWidth: 200,
-    maxWidth: 600,
+    maxWidth: 800,
     height: "100%"
   },
   textField: {
@@ -39,15 +39,18 @@ const styles = theme => ({
   },
 });
 
+const defaultState = {
+  home_atk_rating: 0,
+  home_df_rating: 0,
+  away_atk_rating: 0,
+  away_df_rating: 0,
+  comment: '',
+  player_id: '',
+}
+
 class RatingForm extends Component {
-  state = {
-    home_atk_rating: 0,
-    home_df_rating: 0,
-    away_atk_rating: 0,
-    away_df_rating: 0,
-    comment: '',
-    player_id: '',
-  };
+
+  state = defaultState;
 
   handleChange = (event, inputType) => { 
     this.setState({  
@@ -59,7 +62,7 @@ class RatingForm extends Component {
     event.preventDefault();
     console.log('inside handleSubmit', {...this.state, ...this.props.store.fixtureInfo.info, fixture_id: this.props.fixture_id})
     this.props.dispatch({type:'ADD_RATING', payload: {...this.state, ...this.props.store.fixtureInfo.info, fixture_id: this.props.fixture_id}})
-
+    this.setState(defaultState)
   }
 
   render() {
