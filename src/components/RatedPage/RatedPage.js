a9252adaf2df9f7f7ba011f2a17cd86ff88a9f98
julianbooher <julianbooher@hotmail.com>
2021-01-16 10:32:32 -0600

    added field that displays the user's rating of a current fixture

--- src/components/RatedPage/RatedPage.js
+++ src/components/RatedPage/RatedPage.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import mapStoreToProps from '../../redux/mapStoreToProps';
 import './RatedPage.css';
+
+// Material UI
 import { withStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
+import Button from '@material-ui/core/Button';
+
 
 
 const styles = theme => ({
@@ -21,12 +27,53 @@ const styles = theme => ({
 class RatedPage extends Component {  
   render() {
 
-    const { classes, commentInfo, info } = this.props;
+    const { classes } = this.props;
+    const { info, players, userRating } = this.props.store.fixtureInfo
+
 
     return (
       <div className={classes.root}>
         <Paper className={classes.paper}>
-          <h1>Your Commentary</h1>
+          {JSON.stringify(userRating)}
+          <br/>
+          {JSON.stringify(info)}
+          <div className="rated-page-paper">
+            <h1>Your Rating</h1>
+            <Grid container spacing={1}>
+              <Grid item xs={12} sm={6}>
+                <h3>{info.home_team_name}</h3>
+                {info.home_team_id &&
+                <img alt={info.home_team_name} src={`https://media.api-sports.io/football/teams/${info.home_team_id}.png`}/>
+                }
+                <p><b>Attack Rating:</b> {userRating.home_atk_rating}</p>
+                <p><b>Defense Rating:</b> {userRating.home_df_rating}</p>
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <h3>{info.away_team_name}</h3>
+                {info.away_team_id &&
+                  <img alt={info.away_team_name} src={`https://media.api-sports.io/football/teams/${info.away_team_id}.png`}/>
+                }
+                <p><b>Attack Rating:</b> {userRating.away_atk_rating}</p>
+                <p><b>Defense Rating:</b> {userRating.away_df_rating}</p>
+              </Grid>
+              <Grid item xs={2}></Grid>
+              <Grid item xs={8}>
+                <h3>Comment:</h3>
+                <p>{userRating.comment}</p>
+              </Grid>
+              <Grid item xs={2}></Grid>
+
+            </Grid>
+
+              <Button 
+              variant="contained" 
+              className={classes.button}
+              type="submit">Delete</Button>
+              <Button 
+              variant="contained" 
+              className={classes.button}
+              type="submit">Edit</Button>
+          </div>
         </Paper>
  
       </div>
@@ -34,4 +81,4 @@ class RatedPage extends Component {
   }
 }
 
-export default withStyles(styles)(RatedPage);
+export default connect(mapStoreToProps)(withStyles(styles)(RatedPage));
