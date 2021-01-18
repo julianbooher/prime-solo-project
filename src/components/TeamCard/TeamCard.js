413d506e681af38ed7c9f227e35833e46683099d
julianbooher <julianbooher@hotmail.com>
2021-01-18 11:57:16 -0600

    fixed router parameters bug

--- src/components/TeamCard/TeamCard.js
+++ src/components/TeamCard/TeamCard.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import {withRouter } from 'react-router-dom';
 import mapStoreToProps from '../../redux/mapStoreToProps';
 import { Link } from 'react-router-dom';
 import './TeamCard.css'
@@ -21,8 +22,13 @@ const styles = theme => ({
 });
 
 
-class TeamCard extends Component {
 
+class TeamCard extends Component {
+  
+  handleClick = () => {
+    this.props.history.push(`/team/${this.props.team.id}`)
+    console.log(this.props.team.id)
+  }
 
 
   render() {
@@ -32,17 +38,14 @@ class TeamCard extends Component {
     return (
       <div className="team-card">
         <Paper onClick={this.handleClick} pt={3} pb={3} className={classes.paper}>
-          <Link to ={`/team/${team.id}`}>
             <h4>{team.name}</h4>
             <img alt={team.name} src={`https://media.api-sports.io/football/teams/${team.id}.png`}/>
-            <p>{team.city}</p>
-          </Link>
+          <p>{team.city}</p>
         </Paper>
-
       </div>
 
     );
   }
 }
 
-export default connect(mapStoreToProps)(withStyles(styles)(TeamCard));
+export default connect(mapStoreToProps)(withStyles(styles)(withRouter(TeamCard)));
