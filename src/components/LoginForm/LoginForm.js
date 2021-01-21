d406d08951474858e9d9dabbc05bfbb4c2d6fcf1
julianbooher <julianbooher@hotmail.com>
2021-01-21 10:23:25 -0600

    added material ui to login form

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -3,10 +3,39 @@ import { connect } from 'react-redux';
 import mapStoreToProps from '../../redux/mapStoreToProps';
 import './LoginForm.css';
 
+// Material UI
+import classNames from 'classnames';
+import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import FormControl from '@material-ui/core/FormControl';
+import IconButton from '@material-ui/core/IconButton';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import Visibility from '@material-ui/icons/Visibility';
+import VisibilityOff from '@material-ui/icons/VisibilityOff';
+
+const styles = theme => ({
+  button: {
+    margin: theme.spacing(1),
+  },
+  textField: {
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1),
+    marginTop: 0,
+    width: 200,
+  },
+  withoutLabel: {
+    marginTop: theme.spacing(3),
+  },
+});
+
 class LoginForm extends Component {
   state = {
     username: '',
     password: '',
+    showPassword: false,
   };
 
   login = (event) => {
@@ -31,7 +60,15 @@ class LoginForm extends Component {
     });
   };
 
+  handleClickShowPassword = () => {
+    this.setState(state => ({ showPassword: !state.showPassword }));
+  };
+
   render() {
+
+    const { classes } = this.props;
+
+
     return (
       <form className="formPanel" onSubmit={this.login}>
         <h2>Login</h2>
@@ -41,35 +78,51 @@ class LoginForm extends Component {
           </h3>
         )}
         <div>
-          <label htmlFor="username">
-            Username:
-            <input
-              type="text"
-              name="username"
-              required
-              value={this.state.username}
-              onChange={this.handleInputChangeFor('username')}
-            />
-          </label>
+          <TextField
+            required
+            id="standard-name"
+            label="Username"
+            value={this.state.username}
+            onChange={this.handleInputChangeFor('username')}
+            className={classes.textField}
+            margin="normal"
+          />
         </div>
         <div>
-          <label htmlFor="password">
-            Password:
-            <input
-              type="password"
-              name="password"
-              required
-              value={this.state.password}
-              onChange={this.handleInputChangeFor('password')}
-            />
-          </label>
+          <FormControl className={classNames(classes.margin, classes.textField)}>
+          <InputLabel htmlFor="adornment-password">Password</InputLabel>
+          <Input
+            required
+            id="adornment-password"
+            type={this.state.showPassword ? 'text' : 'password'}
+            value={this.state.password}
+            onChange={this.handleInputChangeFor('password')}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="Toggle password visibility"
+                  onClick={this.handleClickShowPassword}
+                >
+                  {this.state.showPassword ? <Visibility /> : <VisibilityOff />}
+                </IconButton>
+              </InputAdornment>
+            }
+          />
+        </FormControl>
         </div>
         <div>
-          <input className="btn" type="submit" name="submit" value="Log In" />
+        <Button 
+              variant="contained"
+              color="primary" 
+              className={classes.button}
+              type="submit"
+            >
+              Login
+            </Button>
         </div>
       </form>
     );
   }
 }
 
-export default connect(mapStoreToProps)(LoginForm);
+export default connect(mapStoreToProps)(withStyles(styles)(LoginForm));
