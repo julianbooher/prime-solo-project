ac8cc00711a6a592a7bb2c66e266e3d2bf210044
julianbooher <julianbooher@hotmail.com>
2021-01-21 17:32:15 -0600

    made team names and usernames links inside of the comment bubbles

--- src/components/WordBubble/WordBubble.js
+++ src/components/WordBubble/WordBubble.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { withRouter, Link } from 'react-router-dom';
 import './WordBubble.css';
 import { withStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
@@ -19,6 +20,14 @@ const styles = theme => ({
 
 
 class WordBubbles extends Component {  
+  
+
+  goToUser = () => {
+    this.props.history.push(`/user/${this.props.comment.username}`)
+  }
+
+
+
   render() {
 
     const { classes, comment } = this.props;
@@ -28,11 +37,20 @@ class WordBubbles extends Component {
         <Paper className={classes.paper}>
           <Grid container spacing={1}>
               <Grid item xs={12} sm={5} container spacing={2}>
-                <Grid item xs>
-                  <p className="username">{comment.username}</p>
-                  <p className="rating home-team">{comment.home_team_name}<br></br> <b>ATK:</b> {comment.home_atk_rating} <b>DEF:</b>  {comment.home_df_rating}</p>
-                  <p className="rating away-team">{comment.away_team_name}<br></br> <b>ATK:</b>  {comment.away_atk_rating} <b>DEF:</b>  {comment.away_df_rating}</p>
-                  <p className="rating potm">POTM: <br></br>{comment.potm_name}</p>
+                <Grid item xs className="rating-info">
+                  <Link to={`/user/${comment.username}`}>
+                    <h3>{comment.username}</h3>
+                  </Link>
+                  <Link to={`/team/${comment.home_team_id}`}>
+                    <p className="rating home-team">{comment.home_team_name}</p>
+                  </Link> 
+                  <p><b>ATK:</b> {comment.home_atk_rating} <b>DEF:</b>  {comment.home_df_rating}</p>
+                  <Link to={`/team/${comment.away_team_id}`}>
+                    <p className="rating away-team">{comment.away_team_name}</p>
+                  </Link> 
+                  <p><b>ATK:</b>  {comment.away_atk_rating} <b>DEF:</b>  {comment.away_df_rating}</p>
+                  <p className="rating-potm">POTM:</p>
+                  <p>{comment.potm_name}</p>
                 </Grid>
               </Grid>
             <Grid item xs={12} sm={7} container >
@@ -48,4 +66,4 @@ class WordBubbles extends Component {
   }
 }
 
-export default withStyles(styles)(WordBubbles);
+export default withStyles(styles)(withRouter(WordBubbles));
